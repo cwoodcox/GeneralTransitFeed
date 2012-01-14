169c00c9c6f27f9de8a97908a97318116a34b9a5
Corey Woodcox <corey.woodcox@gmail.com>
2012-01-13 18:58:50 -0700

    generate GTFObject class

---
+++ GTFObject.h
@@ -0,0 +1,13 @@
+//
+//  GTFObject.h
+//  GeneralTransitFeed
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GTFObject : NSObject
+
+@end
---
+++ GTFObject.m
@@ -0,0 +1,13 @@
+//
+//  GTFObject.m
+//  GeneralTransitFeed
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import "GTFObject.h"
+
+@implementation GTFObject
+
+@end
