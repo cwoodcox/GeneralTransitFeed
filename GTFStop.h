7245f3e944c20c4e5faab6a564de93dd7ea87c40
Corey Woodcox <corey.woodcox@gmail.com>
2012-01-13 19:07:38 -0700

    generate GTFStop class

---
+++ GTFStop.h
@@ -0,0 +1,13 @@
+//
+//  GTFStop.h
+//  GeneralTransitFeed
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import "GTFObject.h"
+
+@interface GTFStop : GTFObject
+
+@end
---
+++ GTFStop.m
@@ -0,0 +1,13 @@
+//
+//  GTFStop.m
+//  GeneralTransitFeed
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import "GTFStop.h"
+
+@implementation GTFStop
+
+@end
