1529732b9cdcf9caeb89c5e661b0dbbc004c5aaf
Corey Woodcox <corey.woodcox@gmail.com>
2012-01-13 18:56:27 -0700

    Initial Commit

---
+++ GeneralTransitFeedTests/GeneralTransitFeedTests.h
@@ -0,0 +1,13 @@
+//
+//  GeneralTransitFeedTests.h
+//  GeneralTransitFeedTests
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface GeneralTransitFeedTests : SenTestCase
+
+@end
---
+++ GeneralTransitFeedTests/GeneralTransitFeedTests.m
@@ -0,0 +1,32 @@
+//
+//  GeneralTransitFeedTests.m
+//  GeneralTransitFeedTests
+//
+//  Created by Corey Woodcox on 1/13/12.
+//  Copyright (c) 2012 saxton|horne Advertising. All rights reserved.
+//
+
+#import "GeneralTransitFeedTests.h"
+
+@implementation GeneralTransitFeedTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in GeneralTransitFeedTests");
+}
+
+@end
